4a793a55ace2325ffe19103b13a35467510bd295
MikeWentino <mweldon.rpi@gmail.com>
2014-07-28 22:01:55 -0400

    Update Encryptor.java

--- Encryptor.java
+++ Encryptor.java
@@ -1,45 +1,27 @@
 package com.example.textlockapp;
-	import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 import java.util.Scanner;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-//import android.telephony.SmsManager;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.content.Context;
 
 import android.content.res.AssetManager;
 
-	public class Encryptor
-	{
+public class Encryptor
+{
 	  static Random generator = new Random();
 	  static File file = new File("Key3.Txt");
 	  static String[] LIST = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "'", "." };
 	  private Context c;	  
 	  private static String KeyText;
-	  
+	  private static String[][] Key;
+
 	  public Encryptor(Context context)
 	  {
 		  c = context;
-		  
+
 		  byte[] fileBuffer;
 		  try {
 			  AssetManager am = c.getAssets();
@@ -54,11 +36,23 @@ import android.content.res.AssetManager;
 		    // exception handling
 		  }
 		  
-	  }
-	  
-	  private static String cypherCharacter(String a) throws FileNotFoundException
-	  {
+		  //creating Key
 		  Scanner readerKey = new Scanner(KeyText);
+		  Key = new String[65][50];
+		  
+		  for (int i = 0; i < 65; i++)
+		  {
+			  for(int j=0; j<50; i++)
+			  {
+				  Key[i][j] = readerKey.next();
+			  }
+			  readerKey.next();
+		  }
+		  readerKey.close();
+	  }
+
+	  private static int keyLocation(String a)
+	  {
 		  int location = -1;
 		  for (int i = 0; i < 65; i++) {
 			  if (a.equals(LIST[i]))
@@ -67,29 +61,22 @@ import android.content.res.AssetManager;
 				  break;
 			  }
 		  }
-		  if (location == -1) {
-			  return "i";
-		  }
-		  int randNum = generator.nextInt(49);
-		  for (int i = 0; i < location * 51 + randNum; i++) {
-			  readerKey.next();
-		  }
-		  return readerKey.next();
+		  return location;
 	  }
-	  
+
 	  public String encrypt(String input) throws IOException
 	  {
-		  
-	    String output = "";
-	    for (int i = 0; i < input.length(); i++)
-	    {
-	      output = output + cypherCharacter(Character.toString(input.charAt(i)));
-	      if (generator.nextInt(2) == 1) {
-	        output = output + "8";
-	      } else {
-	        output = output + "9";
-	      }
-	    }
-	    return output;
+
+		  String output = "";
+		  for (int i = 0; i < input.length(); i++)
+		  {
+			  output = output + Key[keyLocation(Character.toString(input.charAt(i)))][generator.nextInt(49)];
+			  if (generator.nextInt(2) == 1) {
+				  output = output + "8";
+			  } else {
+				  output = output + "9";
+			  }
+		  }
+		  return output;
 	  }
-	}
+}
