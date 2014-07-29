3d7620c8c5fdf43b84be68165cb414323c1895e0
MikeWentino <mweldon.rpi@gmail.com>
2014-07-29 01:11:29 -0400

    Update Decryptor.java

--- Decryptor.java
+++ Decryptor.java
@@ -1,53 +1,65 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+package com.example.textlockapp;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Scanner;
+
+import android.content.Context;
+import android.content.res.AssetManager;
 
 public class Decryptor
 {
-  public String decrypt(String input)
-    throws FileNotFoundException
-  {
-    File file = new File("Key3.Txt");
-    String input2 = "";String output = "";String temp = "";String temp2 = "";String temp3 = "";
-    Scanner readerKey = new Scanner(file);
-    for (int i = 0; i < input.length(); i++) {
-      if ((Character.toString(input.charAt(i)).equals("8")) || (Character.toString(input.charAt(i)).equals("9"))) {
-        input2 = input2 + " ";
-      } else {
-        input2 = input2 + Character.toString(input.charAt(i));
-      }
+	static String[] LIST = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", "'", "." };
+	private Context c;
+	private String KeyText;
+	private String[][] Key;
+	  
+	public Decryptor(Context context) 
+	{
+		c = context;
+		
+		byte[] fileBuffer;
+		try {
+			AssetManager am = c.getAssets();
+			InputStream fileStream = am.open("Key3.txt");
+			int fileLen = fileStream.available();
+			// Read the entire resource into a local byte buffer.
+			fileBuffer = new byte[fileLen];
+			fileStream.read(fileBuffer);
+			fileStream.close();
+			KeyText = new String(fileBuffer);
+		} catch (IOException e) {
+		    // exception handling
+		}
+		
+		Scanner readerKey = new Scanner(KeyText);
+		Key = new String[65][50];
+
+		for (int i = 0; i < 65; i++)
+		{
+			for(int j=0; j<50; j++)
+			{
+				Key[i][j] = readerKey.next();
+			}
+			readerKey.next();
+		}
+		readerKey.close();
+	}
+	public String decrypt(String input)
+	{
+		String output = "", temp = "";
+		
+		for (int i = 0; i < input.length(); i++) {
+			if ((Character.toString(input.charAt(i)).equals("8")) || (Character.toString(input.charAt(i)).equals("9"))) {
+				for(int j=0; j<65; j++)
+					  for(int k=0; k<50; k++)
+						  if(temp.equals(Key[j][k]))
+							  output = output + LIST[j];
+				temp = "";
+			} else {
+				temp = temp + Character.toString(input.charAt(i));
+			}
+		}
+		return output;
     }
-    Scanner readerInput2 = new Scanner(input2);
-    while (readerInput2.hasNext())
-    {
-      temp = readerInput2.next();
-      
-      int i = 0;
-      readerKey = new Scanner(file);
-      while (readerKey.hasNext())
-      {
-        i++;
-        temp2 = readerKey.next();
-        if (temp.equals("i"))
-        {
-          output = output + "@";
-          break;
-        }
-        if (temp2.equals(temp))
-        {
-          for (int j = 0; j < 50 - i % 51; j++) {
-            readerKey.next();
-          }
-          temp3 = readerKey.next();
-          if (temp3.equals("/\\"))
-          {
-            output = output + " "; break;
-          }
-          output = output + temp3;
-          break;
-        }
-      }
-    }
-    return output;
-  }
 }
